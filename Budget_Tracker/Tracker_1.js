3d3aa5ae0064c6208562b068499e6ad3ced71e80
Sadiq <sadiqict63@gmail.com>
2025-04-22 10:13:36 +0600

    Tracker 1 & 2

---
+++ Budget_Tracker/Tracker_1.js
@@ -0,0 +1,13 @@
+console.log('Personal Budget tracker');
+console.log('Sadiqul Islam');
+console.log(3489.45); //my income
+console.log(2302.45); // my expense
+
+var Username='Sadiq';
+const IncomeAmount=3489.45;
+let ExpenseAmount =2302.45;
+console.log('Personal Budget Tracket Challenge 💪');
+console.log('Username: ' + Username);
+console.log('Income Statement: ' + IncomeAmount);
+console.log('Expense Statement: ' + ExpenseAmount);
+
