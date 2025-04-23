86e305bd83c26ef0a37f2f7c772adf3494d268c5
Sadiq <sadiqict63@gmail.com>
2025-04-23 10:04:11 +0600

    2d done

--- Practice-zone/2D_Array.js
+++ Practice-zone/2D_Array.js
@@ -1,12 +1,32 @@
 //Task-9 Create a function called highestRunScorer that will
 // Receive a 2D array called playersInfo
-// Return the highest score, return the name of the player
+// Based on the highest score, return the name of the player
 
 
 function highestRunScorer(playersInfo) {
     console.log(playersInfo);
-    
-}
+    let MaxScore = playersInfo[0][1];
+    let MaxScorer = playersInfo[0][0];
 
-let playersInfo=[["Sakib",35],["Tamim",56],["Mushfiq",23],["Riad",78],["Mehedi",95],["Mashrafee",67]]
-highestRunScorer(playersInfo)
+    for (let i = 1; i < playersInfo.length; i++) {
+
+        if (MaxScore < playersInfo[i][1]) 
+        {
+            MaxScore = playersInfo[i][1];
+            MaxScorer = playersInfo[i][0];
+        }
+
+    }
+    return MaxScorer;
+}
+    let playersInfo =
+        [
+            ["Sakib", 35],
+            ["Tamim", 56],
+            ["Mushfiq", 23],
+            ["Riad", 78],
+            ["Mehedi", 95],
+            ["Mashrafee", 67]
+        ]
+    var TopScore = highestRunScorer(playersInfo);
+    console.log("The Highest Scorer name is "+TopScore);
