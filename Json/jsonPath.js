935213f553d36e7094cddf6ef23cca8c08d3f6e7
Sadiq <sadiqict63@gmail.com>
2025-04-24 11:22:24 +0600

    JSON DONE

--- Json/jsonPath.js
+++ Json/jsonPath.js
@@ -2,7 +2,6 @@
 // Use for JSON code organize- https://codebeautify.org/jsonviewer
 // Use for find path help- https://jsonpathfinder.com/
 // Use for more fake data - https://jsonplaceholder.typicode.com/
-
 const data=require('./Friends_data.json');
 console.log(data);
 
---
+++ Json/jsonSchema.js
@@ -0,0 +1,3 @@
+//https://www.liquid-technologies.com/online-json-to-schema-converter
+//https://www.jsonschemavalidator.net/
+
