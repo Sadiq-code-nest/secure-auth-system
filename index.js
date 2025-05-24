51bd267deb33b0b9559d74d1fc790522ccad88b4
Sadiq <mdsadiq1221@gmail.com>
2025-05-24 14:17:31 +0600

    basic indexing file added

---
+++ index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const port = process.env.port || 4002;
+app.post('/reg', (req, res) => res.status(200).json({ msg: ('Register page') }));
+app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.use((req, res, next) => res.status(404).json({ msg: 'Route not found' }));
+app.use((err, req, res, next) => res.status(500).json({ msg: 'Something wrong' }));
+app.listen(port, () => console.log(`Server is running at http//localhost.com:${port}`));
