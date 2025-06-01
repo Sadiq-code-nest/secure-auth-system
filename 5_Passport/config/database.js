19cb17b121192f0b03ea4e8dd8caeb039537c576
Sadiq <mdsadiq1221@gmail.com>
2025-06-01 17:41:11 +0600

    config

--- 5_Passport/config/database.js
+++ 5_Passport/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const DataBase_connect = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1/Passport_auth');
+        await mongoose.connect('mongodb://127.0.0.1:27017/Passport_auth');
         console.log('DataBase is connected');
     } catch (error) {
         console.log('DataBase ERROR !!!');
