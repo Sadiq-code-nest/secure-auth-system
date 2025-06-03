9e72cda406320391b672a545119d2534b410e213
Sadiq <mdsadiq1221@gmail.com>
2025-06-03 12:35:11 +0600

    Added index file latest update at 3June

--- index.js
+++ index.js
@@ -4,36 +4,52 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const MongUser = require('./Models/user-model')
 const app = express();
+const md5 = require('md5');
+// console.log(md5('yes!'));
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-const port = process.env.port || 4002;
+const port = process.env.port;
 app.post('/register', async (req, res) => {
     try {
-        const userModel = new MongUser(req.body);
-        await userModel.save();
-        res.status(200).json(userModel);
+        // --------------------------------------------- change here-----------------------------------------------
+        bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+            const userModel = new MongUser(
+                {
+                    email: req.body.email,
+                    password: hash
+                });
+            await userModel.save();
+            res.status(200).json(userModel);
+        });
+
     } catch (error) {
         res.status(500).json(error.message);
     }
 
 });
 
-
 app.post('/login', async (req, res) => {
     try {
 
-        const { email, password } = req.body;
-        const user = await MongooseSchema.findOne({ email: email });
-        if (user && MongooseSchema.password === password) {
-            res.status(200).json({ STATUS: 'Valid user' })
+        // --------------------------------------------changes here ----------------------------------------
+        const email = req.body.email;
+        const password = req.body.password;
+        const user = await MongUser.findOne({ email: email });
+        if (user) {
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (result === true) {
+                    res.status(200).json({ STATUS: 'Valid user' })
+                }
+            });
 
-        } else { res.status(200).json({ STATUS: 'NOT VALID' }) }
-
-
+        }
+        else { res.status(404).json({ STATUS: 'NOT VALID' }) }
     } catch (error) {
-        res.status(404).send.json({ Msg: error.message })
+        res.status(404).json({ Msg: error.message })
     }
 })
 
@@ -49,10 +65,10 @@ const DB_Connection = async () => {
     }
 }
 
-app.post('/login', (req, res) => res.status(200).json({ msg: ('User is log in page') }));
+// app.post('/login', (req, res) => res.status(200).json({ msg: ('User is log in page') }));
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 app.use((req, res, next) => res.status(404).json({ msg: 'Route not found' }));
-app.use((err, req, res, next) => res.status(500).json({ msg: 'Something wrong' }));
+app.use((err, req, res, next) => res.status(500).json({ msg: 'Something wrong in server ' }));
 app.listen(port, async () => {
     await DB_Connection();
     console.log(`Server is running at http//localhost.com:${port}`);
