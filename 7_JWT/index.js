8a326039dcba61e6e71d3d107213ed8b9870bbc2
Sadiq <mdsadiq1221@gmail.com>
2025-06-18 17:32:59 +0600

    database connected and video till 26 minutes

--- 7_JWT/index.js
+++ 7_JWT/index.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+const DataBase_connect = require('./config/database');
 const port = process.env.port || 4001
 const app = require('./app');
-app.listen(port, () => console.log(`Server is running at http://localhost:${port}`));
+app.listen(port, async () => {
+    await DataBase_connect();
+    console.log(`Server is running at http://localhost:${port}`)
+});
