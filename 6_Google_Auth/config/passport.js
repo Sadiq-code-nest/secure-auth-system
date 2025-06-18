2470e2d11da96d1b4f864d836069c64b2bdacb66
Sadiq <mdsadiq1221@gmail.com>
2025-06-18 13:05:24 +0600

    try

--- 6_Google_Auth/config/passport.js
+++ 6_Google_Auth/config/passport.js
@@ -6,8 +6,8 @@ require('dotenv');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    // clientID: process.env.GOOGLE_CLIENT_ID,
+    // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:4001/auth/google/callback"
 },
     async (accessToken, refreshToken, profile, cb) => {
